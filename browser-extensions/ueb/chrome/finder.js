8cf9394fb06489c8ab2e079d6c75610d6b78d1fa
justin.forest <justin.forest@localhost>
2010-08-07 21:55:27 +0400

    Fixed a javascript error.
    Update issue 4
    Status: Fixed

--- browser-extensions/ueb/chrome/finder.js
+++ browser-extensions/ueb/chrome/finder.js
@@ -14,10 +14,9 @@ function findEditor()
 
   // Support for Google Code Wiki.
   if (!editor) {
-    var re1 = new RegExp("(http://code\.google\.com/p/[^/]+/)wiki/([^?#]+)");
-    var re2 = re1.exec(window.location.href);
-    if (re2)
-      editor = re2[1] + 'w/edit/' + re2[2];
+    var m = /^(http:\/\/code\.google\.com\/p\/[^\/]+\/)wiki\/([^?#]+)/.exec(window.location.href||"");
+    if (m)
+      editor = m[1] + 'w/edit/' + m[2];
   }
 
   if (editor)
