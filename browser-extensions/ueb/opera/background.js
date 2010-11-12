1c439a94ea4a50f9c00cfa4b878488b7b1f85e5f
justin.forest <justin.forest@localhost>
2010-11-12 22:26:12 +0300

    Universal Edit Button for Opera.

---
+++ browser-extensions/ueb/opera/background.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", function(){
+	var button = opera.contexts.toolbar.createItem({
+		disabled: true,
+		title: "Edit this page",
+		icon: "icons/UEB16.png",
+		onclick: function () {
+			var tab = opera.extension.tabs.getFocused();
+			if (tab) {
+				tab.postMessage({ 'name': 'edit' });
+			}
+		}
+	});
+
+	opera.contexts.toolbar.addItem(button);
+
+	function enableButton() {
+		button.disabled = true;
+		var tab = opera.extension.tabs.getFocused();
+		if (tab) {
+			tab.postMessage({ 'name': 'check' });
+		}
+	}
+
+	opera.extension.onconnect = enableButton;
+	opera.extension.tabs.onfocus = enableButton;
+	opera.extension.tabs.onblur = enableButton;
+
+	opera.extension.onmessage = function (event) {
+		if (event.data.name == 'enable') {
+			// opera.postError('URL: ' + event.data.url);
+			if (event.data.url)
+				button.disabled = false;
+			else
+				button.disabled = true;
+		}
+	};
+}, false);
