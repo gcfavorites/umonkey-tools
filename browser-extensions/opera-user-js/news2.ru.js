f4c74aca5f9cb265972fe1d592efb6a70b9c3614
justin.forest <justin.forest@localhost>
2011-12-07 20:11:02 +0300

    Fixes for news2.ru in Opera.

---
+++ browser-extensions/opera-user-js/news2.ru.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @include http://news2.ru/*
+// ==/UserScript==
+//
+// Pins the vote box to the top of news2.ru story pages, so that you wouldn't
+// forget to vote after reading the comments.
+//
+// code.umonkey.net
+
+(function () {
+	var init_css = true;
+
+	function on_scroll() {
+		if (init_css) {
+			css = document.createElement("style");
+			css.type = "text/css";
+			css.innerHTML = ".vote_placeholder.pinned { position: fixed; top: 10px } .news_body { margin-left: 60px }";
+			document.body.appendChild(css);
+			init_css = false;
+		}
+
+		try {
+			var div = document.getElementsByClassName("vote_placeholder");
+			if (!div.length)
+				return;  // layout changed
+
+			var y = document.documentElement.scrollTop;
+
+			if (y > 160) {
+				div[0].className = "vote_placeholder pinned";
+			} else {
+				div[0].className = "vote_placeholder";
+			}
+		} catch (e) {
+			alert(e);
+		}
+	}
+
+	window.addEventListener("scroll", on_scroll, false);
+})();
